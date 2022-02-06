1438cd28f6b5a98c5bdfc4d51b43105a5b2b1d3e
Ludwig Magnusson <ludwig@mediatool.com>
2022-02-06 12:23:38 +0100

    display dmenu in list mode

--- config.h
+++ config.h
@@ -80,7 +80,7 @@ static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "togg
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-l", "10", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "-e", "dvtm" };
 static const char *chromecmd[]  = { "google-chrome-stable", NULL };
 static const char *slackcmd[]  = { "slack", NULL };
