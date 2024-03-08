89b70a974f8db258b50bce1e008d399985bcccb3
Ludwig Magnusson <ludwig@mediatool.com>
2024-03-08 09:46:10 +0100

    rice the borders

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
